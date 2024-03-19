710b3ef1ae94da122b35e507099cc7753967de9e
HelloBell <2535794625@qq.com>
2024-03-19 15:14:39 +0800

    角色页面的开发

--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="role">
-    <page-search :search-config="searchConfig" />
+    <page-search
+      :search-config="searchConfig"
+      @query-click="handleQueryBtnClick"
+      @reset-click="handleResetBtnClick"
+    />
+    <page-content
+      :content-config="contentConfig"
+      ref="pageContentRef"
+      @new-click="handleNewBtnClick"
+      @edit-click="handleEditBtnClick"
+    />
+    <page-modal :modal-config="modalConfig" ref="pageModalRef" />
   </div>
 </template>
 
 <script setup lang="ts">
 import PageSearch from '@/components/page-search/page-search.vue'
 import searchConfig from './config/search.config'
+
+import PageContent from '@/components/page-content/page-content.vue'
+import contentConfig from './config/content.config'
+
+import PageModal from '@/components/page-modal/page-modal.vue'
+import modalConfig from './config/modal.config'
+import usePageContent from '@/hooks/usePageContent'
+import usePageModal from '@/hooks/usePageModal'
+
+// 逻辑关系
+const { pageContentRef, handleQueryBtnClick, handleResetBtnClick } =
+  usePageContent()
+const { pageModalRef, handleNewBtnClick, handleEditBtnClick } = usePageModal()
 </script>
 
 <style lang="less" scoped>
